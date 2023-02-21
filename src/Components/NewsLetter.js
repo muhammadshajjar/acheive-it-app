e321a30568333d5776b512d5c9bc0ca76a3aa3bc
shajjar <muhammadshajjar99@gmail.com>
2023-02-21 22:48:27 +0500

    added the newsletter subscription service

--- src/Components/NewsLetter.js
+++ src/Components/NewsLetter.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 import "./NewsLetter.css";
 
 import mailBoxIllustration from "../Assets/mailbox.png";
+import useLoader from "../Hooks/useLoader";
+
+//for emailjs
+const SERVICE_ID = "service_8ha85yf";
+const TEMPLATE_ID = "template_bagdq3p";
+const API_KEY = "HhA-gjSCWsIVaC0tu";
 
 const NewsLetter = () => {
+  const [email, setEmail] = useState("");
+  const [loader, isLoading, setIsLoading] = useLoader();
+  const form = useRef();
+
+  const emailSubmitHandler = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, API_KEY).then(
+      (result) => {
+        if (result.text === "OK") {
+          setIsLoading(false);
+          alert("Email Sent Successfully!");
+        }
+      },
+      (error) => {
+        console.log(error.text);
+        setIsLoading(false);
+        alert("Failed sending email!");
+      }
+    );
+
+    setEmail("");
+  };
   return (
     <section className="container">
       <div className="newsletter">
@@ -20,14 +50,21 @@ const NewsLetter = () => {
             Be the first to know about our new products, services, and upcoming
             events by subscribing to our newsletter
           </p>
-          <div className="newsletter__content--sub">
+          <form
+            className="newsletter__content--sub"
+            onSubmit={emailSubmitHandler}
+            ref={form}
+          >
             <input
               type="email"
-              name="newsletterEmail"
               placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
+              name="user_email"
+              value={email}
             />
             <button>Subscribe</button>
-          </div>
+            {loader}
+          </form>
         </div>
       </div>
     </section>
